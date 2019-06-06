821f8fb14ee7f02f1e06f821dee3d63a67ff3620
matkuh <matkuh@gmail.com>
2019-06-06 15:53:41 -0700

    did most of the assignment in one sitting and completely forgot to commit, will try to add more for styling and put in some pseudo code in js file. create a function to display gifs. In the function there is an ajax call that accesses the giphy api.  Put a for loop in the call that generates a imgdiv for the gif and a <p> for the rating.  give img div attribute of data-state animated and still. the prepend those on the gif container on html. also create a onclick function that changes the data states of each gif and gets the proper state(still or animated)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,82 @@
-$("#gifbutton").on("click", function() {
-    event.preventDefault();
+var gifArray = ["Whale", "Shark", "Fish", "Squid"]
 
-var gif = $("#gif-input").val().trim();
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=6sd1wi2IPBy3XgJWTFEsPcQMhYxYrLQS&limit=5";
-    
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-    .then(function(response) {
-var results = response.data
-
-$("#gifscont").empty();
-
-for (var i = 0; i < results.length; i++) {
-    var gifDiv = $("<div>");
-
-    var rating = results[i].rating;
-
-    var p = $("<p>").text("Rating: " + rating);
-
-    var personImage = $("<img>");
-    personImage.attr("src", results[i].images.fixed_height.url);
-
-    gifDiv.prepend(p);
-    gifDiv.prepend(personImage);
-
-    $("#gifscont").prepend(gifDiv);
+function renderButtons() {
+    $("#button-container").empty();
+    for (var i=0; i < gifArray.length; i++){
+        var newButton = $("<button>");
+        newButton.addClass("gifbutton");
+        newButton.attr("data-name", gifArray[i]);
+        
+        newButton.text(gifArray[i]);
+        $("#button-container").append(newButton);
+    }
 }
 
-})
-})
+function alertGifName(){
+    var gifName = $(this).attr("data-name")
+}
+
+function displayGifs(){
+
+    var gif = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=6sd1wi2IPBy3XgJWTFEsPcQMhYxYrLQS&limit=10";
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function(response) {
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+                
+                var rating = results[i].rating;
+                
+                var p = $("<p>").text("Rating: " + rating);
+
+                var gifImage = $("<img>");
+                gifImage.attr("src", results[i].images.fixed_height_still.url);
+                gifImage.attr("data-state", "still")
+                gifImage.attr("data-animate", results[i].images.fixed_height.url)
+                gifImage.attr("data-still", results[i].images.fixed_height_still.url)
+                gifImage.addClass("gifImg");
+
+                gifDiv.prepend(p);
+                gifDiv.prepend(gifImage);
+
+                $("#gifscont").prepend(gifDiv);  
+                console.log(results)
+            }
+        })
+    }
+
+$("#gifbutton").on("click", function(event) {
+    event.preventDefault();
+    var gif = $("#gif-input").val().trim();
+    gifArray.push(gif)
+    renderButtons();
+});
+
+$(document).on("click", ".gifbutton", displayGifs);
+
+renderButtons();
+
+$(document).on("click", ".gifImg", function(event){
+    var state = $(this).attr("data-state")
+    // console.log(state);
+    if (state === "still"){
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate")
+    }
+    else if (state === "animate"){
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still")
+    }
+
+});
+
+
+
+
+
