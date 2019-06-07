6532cf08b097d882c83623cf7fcf9f31dcf09413
matkuh <matkuh@gmail.com>
2019-06-06 20:38:27 -0700

    add some css to style the buttons, color, resize and a hover thing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,37 @@
+// create an array to store premade buttons and user created buttons 
 var gifArray = ["Whale", "Shark", "Fish", "Squid"]
-
+// create a button render function that will dynamically generate a button for every string in the array.  
 function renderButtons() {
     $("#button-container").empty();
-    for (var i=0; i < gifArray.length; i++){
+    for (var i = 0; i < gifArray.length; i++) {
         var newButton = $("<button>");
         newButton.addClass("gifbutton");
         newButton.attr("data-name", gifArray[i]);
-        
         newButton.text(gifArray[i]);
         $("#button-container").append(newButton);
     }
 }
 
-function alertGifName(){
-    var gifName = $(this).attr("data-name")
-}
-
-function displayGifs(){
-
+// create a function that will display the gifs when a button is pressed 
+function displayGifs() {
+// define a variable equal to the attribute of a button, in this case, the actual text or search
     var gif = $(this).attr("data-name");
+// 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=6sd1wi2IPBy3XgJWTFEsPcQMhYxYrLQS&limit=10";
-    
+
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-        .then(function(response) {
+        .then(function (response) {
             var results = response.data;
 
             for (var i = 0; i < results.length; i++) {
+
                 var gifDiv = $("<div>");
-                
+
                 var rating = results[i].rating;
-                
+
                 var p = $("<p>").text("Rating: " + rating);
 
                 var gifImage = $("<img>");
@@ -45,13 +44,13 @@ function displayGifs(){
                 gifDiv.prepend(p);
                 gifDiv.prepend(gifImage);
 
-                $("#gifscont").prepend(gifDiv);  
+                $("#gifscont").prepend(gifDiv);
                 console.log(results)
             }
         })
-    }
+}
 
-$("#gifbutton").on("click", function(event) {
+$("#gifbutton").on("click", function (event) {
     event.preventDefault();
     var gif = $("#gif-input").val().trim();
     gifArray.push(gif)
@@ -62,14 +61,14 @@ $(document).on("click", ".gifbutton", displayGifs);
 
 renderButtons();
 
-$(document).on("click", ".gifImg", function(event){
+$(document).on("click", ".gifImg", function (event) {
     var state = $(this).attr("data-state")
     // console.log(state);
-    if (state === "still"){
+    if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate")
     }
-    else if (state === "animate"){
+    else if (state === "animate") {
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still")
     }
